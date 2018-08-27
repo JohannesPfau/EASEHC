caaca19468691d392eedd514c6a2323964f5f2e8
JohannesPfau <Johannes.Pfau@gmx.net>
2018-08-27 16:43:09 +0200

    +Simple Assets | Rework Interior

---
+++ EASEUnityProject/Assets/Scripts/TrackingEvent.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackingEvent : MonoBehaviour {
+
+    public DateTime timestamp;
+    public TrackingEventType eventType;
+    public GameObject[] relatedObjects;
+    public Vector3[] objectPositions;
+    public Vector3[] objectRotations;
+
+    public TrackingEvent(string type, GameObject[] relatedObjects)
+    {
+        eventType = (TrackingEventType)Enum.Parse(typeof(TrackingEventType), type);
+        timestamp = DateTime.Now;
+        this.relatedObjects = relatedObjects;
+
+        objectPositions = new Vector3[relatedObjects.Length];
+        int i = 0;
+        foreach(GameObject go in relatedObjects)
+        {
+            Vector3 position = go.transform.position;
+            objectPositions[i] = position;
+            i++;
+        }
+
+        objectRotations = new Vector3[relatedObjects.Length];
+        i = 0;
+        foreach (GameObject go in relatedObjects)
+        {
+            Vector3 rotation = go.transform.rotation.eulerAngles;
+            objectRotations[i] = rotation;
+            i++;
+        }
+    }
+
+    public TrackingEvent(TrackingEventType type, params GameObject[] relatedObjects)
+    {
+        eventType = type;
+        timestamp = DateTime.Now;
+        this.relatedObjects = relatedObjects;
+
+
+        objectPositions = new Vector3[relatedObjects.Length];
+        int i = 0;
+        foreach (GameObject go in relatedObjects)
+        {
+            Vector3 position = go.transform.position;
+            objectPositions[i] = position;
+            i++;
+        }
+
+        objectRotations = new Vector3[relatedObjects.Length];
+        i = 0;
+        foreach (GameObject go in relatedObjects)
+        {
+            Vector3 rotation = go.transform.rotation.eulerAngles;
+            objectRotations[i] = rotation;
+            i++;
+        }
+    }
+
+    public enum TrackingEventType
+    {
+        MOVEMENT,
+        PICKUP,
+        COLLISION,
+        COLLISION_WHILE_HELD,
+        PUTDOWN,
+        THROW
+    }
+}
---
+++ EASEUnityProject/Assets/TrackingLogic.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackingLogic : MonoBehaviour {
+
+    List<TrackingEvent> eventList;
+    public bool recording;
+    public GameObject player;
+    public GameObject playerHandL;
+    public GameObject playerHandR;
+
+    public GameObject test;
+
+    public float movementRecordDelay = 0.1f;
+    float recordDelayed = 0;
+
+    private void Start()
+    {
+        eventList = new List<TrackingEvent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadMinus))
+            recording = false;
+        if (Input.GetKeyDown(KeyCode.KeypadPlus))
+            recording = true;
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+            displayList();
+
+        if (recording && player != null && playerHandL != null && playerHandR != null && recordDelayed > movementRecordDelay)
+        {
+            recordDelayed = 0f;
+            // track player movement
+            trackPositions();
+        }
+        else
+            recordDelayed += Time.deltaTime;
+    }
+    
+    public void trackEvent(string type, params GameObject[] relatedObjects)
+    {
+        eventList.Add(new TrackingEvent(type, relatedObjects));
+    }
+
+    public void trackPositions()
+    {
+        eventList.Add(new TrackingEvent(TrackingEvent.TrackingEventType.MOVEMENT, player, playerHandL, playerHandR));
+    }
+
+    public void displayList()
+    {
+        foreach (TrackingEvent evt in eventList)
+        {
+            for(int i = 0; i < evt.relatedObjects.Length; i++)
+            {
+                string locationdata = "";
+                if (evt.objectPositions.Length >= i)
+                {
+                    locationdata += " POS: " + evt.objectPositions[i] + " ROT: " + evt.objectRotations[i];
+                }
+
+                Debug.Log(evt.timestamp + " (" + evt.eventType.ToString() + ") " + evt.relatedObjects[i].name + locationdata);
+            }
+        }
+    }
+}
