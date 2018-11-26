3fc5fb0a138e2a2fa1c389f58d8c5fa2ef092bde
m-u-d <Johannes.Pfau@gmx.net>
2018-11-26 17:14:32 +0100

    PipelineInput handled (json) | JsonDotNet lib

---
+++ EASEUnityProject/Assets/PipelineInput.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class PipelineInput : MonoBehaviour {
+
+    public string jsonFileName = "outputSebastian_20181115.json";
+
+    // Use this for initialization
+    void Start () {
+        string jString = File.ReadAllText("Assets/PipelineSim/"+ jsonFileName);
+        PipelineInputData jData = JsonConvert.DeserializeObject<PipelineInputData>(jString);
+
+        Debug.Log(jData.version);
+        Debug.Log(jData.application);
+        Debug.Log(jData.uuid);
+        Debug.Log(jData.sentence);
+        Debug.Log("--- PARSES ---");
+        foreach (string s in jData.parses.Keys)
+            Debug.Log(s + ": " + jData.parses[s]);
+        Debug.Log(jData.http_status);
+        Debug.Log("--- GRAPHS ---");
+        foreach (string s in jData.graphs.Keys)
+            Debug.Log(s + ": " + jData.graphs[s]);
+        Debug.Log("--- JSON PARSES ---");
+        Debug.Log(jData.json_parses.smain.__class__);
+        Debug.Log(jData.json_parses.smain.name);
+
+        foreach (jRole r in jData.json_parses.smain.roles)
+            debugRole(r);
+    }
+
+    void debugRole(jRole r)
+    {
+        if (r.__class__ != null)
+            Debug.Log(r.__class__ + ": " + r.type);
+        if (r.target != null)
+            if(r.target.type != null)
+            {
+                Debug.Log("target: " + r.target.type);
+                debugRole(r.target);
+            }
+            else
+                Debug.Log("alt target: " + r.target.alternativeTarget);
+
+        //if (r.alternativeTarget != null && r.alternativeTarget != "")
+    }
+
+}
---
+++ EASEUnityProject/Assets/Scripts/PipelineInputData.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Collections.Generic;
+
+[System.Serializable]
+public class PipelineInputData
+{
+    public string version;
+    public string application;
+    public string uuid;
+    public string sentence;
+    public Dictionary<string, string> parses = new Dictionary<string, string>();
+    public string http_status;
+    public jsonParses json_parses;
+    public Dictionary<string, string> graphs = new Dictionary<string, string>();
+    public string error;
+}
+
+public class jsonParses
+{
+    public sMain smain;
+}
+
+public class sMain
+{
+    public string __class__;
+    public string name;
+    public List<jRole> roles = new List<jRole>() { };
+}
+
+[System.Serializable]
+public class jRole
+{
+    public string __class__;
+    public string type;
+    public jRole target;
+    public string alternativeTarget;
+
+    public static explicit operator jRole(string s)
+    {
+        jRole role = new jRole();
+        role.alternativeTarget = s;
+        return role;
+    }
+}
+
+[System.Serializable]
+public class jVariable
+{
+    public string __class__;
+    public string name;
+    public List<jRole> roles = new List<jRole>() { };
+    public string type;
+}
+
